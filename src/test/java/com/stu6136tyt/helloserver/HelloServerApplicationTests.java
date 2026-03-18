97a7c14d90150ef77c3670d5fca00c93ef60af9a
Tyytt <2665304460@qq.com>
2026-03-18 19:02:48 +0800

    Initial commit1

---
+++ src/test/java/com/stu6136tyt/helloserver/HelloServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stu6136tyt.helloserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
